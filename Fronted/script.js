eec1e9aa0e13ccb51494c4908f7c1481c959afdb
Blackpanther272 <rohitrayatsingh@gmail.com>
2026-08-18 13:13:14 +0530

    Update script.js

--- Fronted/script.js
+++ Fronted/script.js
@@ -1014,26 +1014,7 @@ clearBtn?.addEventListener("click", () => {
 });
 
 
-const themeBtn = document.getElementById("themeToggle");
 
-if (localStorage.getItem("theme") === "dark") {
-  document.body.classList.add("dark-mode");
-  themeBtn.textContent = "☀";
-}
-
-themeBtn?.addEventListener("click", () => {
-
-  document.body.classList.toggle("dark-mode");
-
-  if (document.body.classList.contains("dark-mode")) {
-    localStorage.setItem("theme", "dark");
-    themeBtn.textContent = "☀";
-  } else {
-    localStorage.setItem("theme", "light");
-    themeBtn.textContent = "🌙";
-  }
-
-});
 
 // ============================================================
 // DAMAGE REPORT SYSTEM
